65bdfb317690fe98f4ee9c7e73c890a10267f88e
Andrea8802 <andreabonaventura802@gmail.com>
2022-12-16 11:43:17 +0100

    Creato Header con titolo e barradi ricerca

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,35 @@
+<script>
+
+</script>
+
+<template>
+
+  <header>
+    <h1>
+      BoolFlix
+    </h1>
+    <div class="search">
+      <input type="search">
+      <button>
+        Cerca
+      </button>
+    </div>
+  </header>
+
+</template>
+
+<style lang="scss" scoped>
+header {
+  background-color: black;
+  width: 100%;
+  height: 100px;
+  padding: 0 30px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  h1 {
+    color: red;
+  }
+}
+</style>
